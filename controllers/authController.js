279ada2828a271651be7c3cce9b1fbd2e5518840
Christian Reyes <christian_arb.91@hotmail.com>
2022-02-06 21:50:12 -0600

    Recuperar password. Token y formulario

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,9 @@
 const passport = require('passport');
 const Usuarios = require('../models/Usuarios');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const crypto = require('crypto');
+const bcrypt = require ('bcrypt-nodejs');
 
 //autenticar el usuario
 exports.autenticarUsuario = passport.authenticate('local',{
@@ -35,10 +39,68 @@ exports.enviarToken = async (req, res) => {
 
     //Si no existe el usuario
     if(!usuario){
-        req.flash('error', 'No existe esa cuenta');
-        res.render('reestablecer',{
-            nombrePagina: 'Reestablecer tu Contraseña',
-            mensajes: req.flash()
-        })
+        req.redirect('/reestablecer');
     }
+
+    //usuario existe
+    usuario.token = crypto.randomBytes(20).toString('hex');
+    usuario.expiracion = Date.now() + 3600000;
+
+    //guardarlos en la base de datos    
+    await usuario.save();
+
+    //url de reset
+    const resetUrl = `http://${req.headers.host}/reestablecer/${usuario.token}`;
+
+}
+
+exports.validarToken = async (req, res) => {
+    const usuario = await Usuarios.findOne({
+        where:{
+            token: req.params.token
+        }
+    });
+
+    //Si no encuentra el usuario
+    if(!usuario){
+        req.flash('error', 'No válido');
+        res.redirect('/reestablecer');
+    }
+    
+    //Formulario para generar el password
+    res.render('resetPassword', {
+        nombrePagina: 'Reestablecer Contraseña'
+    })
+}
+
+//Cambia el password por uno nuevo
+exports.actualizarPassword = async (req, res) => {
+    //Verifica token valido y fecha de expiración
+    const usuario = await Usuarios.findOne({
+        where: {
+            token: req.params.token,
+            expiracion: {
+                [Op.gte]: Date.now()
+            }
+        }
+    });
+
+    //verificar si el usuario existe
+    if(!usuario){
+        req.flash('error', 'No válido');
+        res.redirect('/reestablecer');
+    }
+
+    //Hashear el nuevo password
+
+    usuario.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    usuario.token = null;
+    usuario.expiracion = null;
+    
+    //Guardar el nuevo password
+    await usuario.save();
+
+    req.flash('correcto', 'Tu password se ha modificado correctamente');
+    res.redirect('/iniciar-sesion');
+
 }
